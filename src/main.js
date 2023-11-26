882dfd40c6ae9a5df8ee744a90f0ac14cfdb0640
Minkhantnaung@securelink <minkhantnaung@securelinkmm.com>
2023-11-26 21:19:06 +0630

    set up pinia

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import './assets/main.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
+
+app.use(pinia)
 
 app.mount('#app')
