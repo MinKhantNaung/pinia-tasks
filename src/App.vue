6881909265ce3dd86c63822e3ccc55ce2808542d
Minkhantnaung@securelink <minkhantnaung@securelinkmm.com>
2023-11-26 21:14:34 +0630

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="d-flex justify-content-center align-items-center">
+    <img src="./assets/pinia_logo.png" alt="pinia logo">
+    <h1>Pinia Tasks</h1>
+  </div>
+</template>
